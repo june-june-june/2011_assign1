0f6b4f0a4d079d7fb9a0dd728ca89e70491a1e4f
June <57237093+june-june-june@users.noreply.github.com>
2020-06-27 16:56:23 -0400

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -12,8 +12,38 @@ public class Assignment1{
 	  if (size == 1) {
 		  C[0][0] = A[0][0] * B[0][0];
 	  } else {
-		  int[][] M0 = denseMatrixMult(sum(A, A, 0, 0, size/2, size/2, size/2), sum(B, B, 0, 0, size/2, size/2, size/2), size/2);
+		  int[][] zero = new int[size][size]; //used to split matrix into sub-matrices
 		  
+		  /*
+		   * M0 = (A00 + A11) * (B00 + B11)
+		   * M1 = (A10 + A11) * B00
+		   * M2 = A00 * (B01 - B11)
+		   * M3 = A11 * (B10 - B00)
+		   * M4 = (A00 + A01) * B11
+		   * M5 = (A10 - A00) * (B00 + B01)
+		   * M6 = (A01 - A11) * (B10 + B11)
+		   */
+		  
+		  int[][] M0 = denseMatrixMult(sum(A, A, 0, 0, 1, 1, size/2), sum(B, B, 0, 0, 1, 1, size/2), size/2);
+		  int[][] M1 = denseMatrixMult(sum(A, A, 1, 0, 1, 1, size/2), sum(B, zero, 0, 0, 0, 0, size/2), size/2);		  
+		  int[][] M2 = denseMatrixMult(sum(A, zero, 0, 0, 0, 0, size/2), sub(B, B, 0, 1, 1, 1, size/2), size/2);
+		  int[][] M3 = denseMatrixMult(sum(A, zero, 1, 1, 0, 0, size/2), sub(B, B, 1, 0, 0, 0, size/2), size/2);
+		  int[][] M4 = denseMatrixMult(sum(A, A, 0, 0, 0, 1, size/2), sum(B, zero, 1, 1, 0, 0, size/2), size/2);
+		  int[][] M5 = denseMatrixMult(sub(A, A, 1, 0, 0, 0, size/2), sum(B, B, 0, 0, 0, 1, size/2), size/2);
+		  int[][] M6 = denseMatrixMult(sub(A, A, 0, 1, 1, 1, size/2), sum(B, B, 1, 0, 1, 1, size/2), size/2);
+		  
+		  /*
+		   * C00 = M0 + M3 - M4 + M6
+		   * C01 = M2 + M4
+		   * C10 = M1 + M3
+		   * C11 = M0 - M1 + M2 + M5
+		   */ 
+		  
+		  for (int i = 0; i < size; i++) {
+			  for (int j = 0; j < size; j++) {
+				  
+			  }
+		  }
 	  }
 	  
 	  return C;
@@ -22,18 +52,10 @@ public class Assignment1{
   public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
   {
 	  int[][] C = new int[n][n];
-	  int ay = y1;
-	  int by = y2;
-	  for (int i = 0; i < n; i++) {
-		  ay = y1;
-		  by = y2;
-		  for (int j = 0; j < n; j++) {
+	  for (int i = 0; i < n; i++, x1++, x2++) {
+		  for (int j = 0, ay = y1, by = y2; j < n; j++, ay++, by++) {
 			  C[i][j] = A[x1][ay] + B[x2][by];
-			  ay++;
-			  by++;
 		  }
-		  x1++;
-		  x2++;
 	  }
 	  return C;
   }
@@ -41,18 +63,10 @@ public class Assignment1{
   public int[][] sub(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
   {
 	  int[][] C = new int[n][n];
-	  int ay = y1;
-	  int by = y2;
-	  for (int i = 0; i < n; i++) {
-		  ay = y1;
-		  by = y2;
-		  for (int j = 0; j < n; j++) {
+	  for (int i = 0; i < n; i++, x1++, x2++) {
+		  for (int j = 0, ay = y1, by = y2; j < n; j++, ay++, by++) {
 			  C[i][j] = A[x1][ay] - B[x2][by];
-			  ay++;
-			  by++;
 		  }
-		  x1++;
-		  x2++;
 	  }
 	  return C;
   }
